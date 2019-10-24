d01890baaa4ee6b95b28d2f89b3792162cf2cd31
Gustavo_Matos <gustavo.matos@smn.com.br>
2019-10-24 15:12:03 -0300

    HTML da página principal implementado

---
+++ src/pages/main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    max-width: 1224px;
+    background: #eee;
+`;
+
+export const Main = styled.main``;
+
+export const Section = styled.section``;
+
+export const Footer = styled.footer``;
